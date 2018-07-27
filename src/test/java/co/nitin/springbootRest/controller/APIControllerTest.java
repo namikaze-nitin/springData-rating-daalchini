9102942ed1d39b9329ae74bc341e3ad79df6ae34
Nitin Sharma <nitinsharmares@gmail.com>
2018-07-27 15:07:49 +0530

    Test case added for a case when ratings corresponding to an orderNo already exist.

--- src/test/java/co/nitin/springbootRest/controller/APIControllerTest.java
+++ src/test/java/co/nitin/springbootRest/controller/APIControllerTest.java
@@ -33,6 +33,7 @@ import co.nitin.springbootRest.model.OrderResponse;
 import co.nitin.springbootRest.model.Orders;
 import co.nitin.springbootRest.model.Products;
 
+
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = WebEnvironment.DEFINED_PORT)
 @AutoConfigureMockMvc
@@ -47,8 +48,9 @@ public class APIControllerTest {
 	@Autowired private OrdersDAO ordersDao;
 	@Autowired private MockMvc mockMvc;
 	@Autowired ObjectMapper mapper;
+	
 	@Test
-	public void testControllerWorkingJson() throws Exception {
+	public void testControllerWorkingJson() throws Exception {//Ratings and Request are not already saved
 
 		//Saving product
 		Products prod = new Products();
@@ -111,4 +113,79 @@ public class APIControllerTest {
 		OrderResponse orderResponse = mapper.readValue(response, OrderResponse.class);
 		Assert.assertEquals(orderResponse.getStatus(),"SUCCESS");
 	}
+	
+	@Test
+	public void testControllerRatingsError() throws Exception {//Ratings and Request are not already saved
+
+		//Saving product
+		Products prod = new Products();
+		prod.setVariant_id("xxxx");
+		prod.setVariant_name("Aaloo Chips");
+		productdao.save(prod);
+		
+		prod.setVariant_id("yyyy");
+		prod.setVariant_name("Lays");
+		productdao.save(prod);
+		prod.setVariant_id("zzzz");
+		prod.setVariant_name("Sandwich");
+		productdao.save(prod);
+
+		//Saving order
+		Orders order = new Orders();
+		order.setOrderNo("aaaa");
+		order.setTotalCost(12345);
+		ordersDao.save(order);
+		
+		List <OrderRatings>list = new ArrayList<>();
+				
+		//Saving OrderRequest
+		OrderRequest ordReq = new OrderRequest();
+		ordReq.setApiName("giveitemrating");
+		ordReq.setApiVersion(1.0);
+		ordReq.setOrderNo("aaaa");
+		ordReq.setRateOverAllExperience(4);
+		
+		//Saving Ratings
+		OrderRatings ordRat1 = new OrderRatings();
+		ordRat1.setRating_food((byte) 1);
+		ordRat1.setVariant_id("xxxx");
+		list.add(ordRat1);
+
+		OrderRatings ordRat2 = new OrderRatings();
+		ordRat2.setRating_food((byte) 1);
+		ordRat2.setVariant_id("yyyy");
+		list.add(ordRat2);
+		
+		OrderRatings ordRat3 = new OrderRatings();
+		ordRat3.setRating_food((byte) 0);
+		ordRat3.setVariant_id("zzzz");
+		list.add(ordRat3);
+		
+		ordRat1.setRequest(ordReq);
+		ordRat2.setRequest(ordReq);
+		ordRat3.setRequest(ordReq);
+
+		ordReq.setRatings(list);
+		orderRequestdao.save(ordReq);
+		orderRatingsdao.save(list);
+		
+		Assert.assertNotNull(mockMvc);		
+		
+		String response = (mockMvc.perform(post("/rating")			//POST request link
+					.contentType("application/json;charset=UTF-8")	//Request data format
+					.content(mapper.writeValueAsString(ordReq)))	//JSON to hit the API
+			.andExpect(status().isOk())								//Check for successful response hit:202
+			.andExpect(content()									//Set response related data int addExpect
+					.contentType("application/json;charset=UTF-8"))	//Expected response format
+			.andReturn()											//Fetch ALL data
+			.getResponse()											//Fetch response from data
+			.getContentAsString()									//fetch all content in string format
+			);
+		
+		OrderResponse orderResponse = mapper.readValue(response, OrderResponse.class);
+		Assert.assertEquals(orderResponse.getStatus(),"ERROR");
+		Assert.assertEquals(orderResponse.getStatusMessage(), "ER_RATINGS_0001");
+	}
+	
+	
 }
