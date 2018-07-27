1f1fdbc86bc1d06cb580f0d8cc2517467667a1cb
Nitin Sharma <nitinsharmares@gmail.com>
2018-07-27 12:23:38 +0530

    Refactored Code

--- src/test/java/co/nitin/springbootRest/controller/DAOPersistTest.java
+++ src/test/java/co/nitin/springbootRest/controller/DAOPersistTest.java
@@ -9,15 +9,11 @@ import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.servlet.MockMvc;
-
 import co.nitin.springbootRest.dao.OrderRatingsDAO;
 import co.nitin.springbootRest.dao.OrderRequestDAO;
-import co.nitin.springbootRest.dao.OrdersDAO;
 import co.nitin.springbootRest.dao.ProductsDAO;
 import co.nitin.springbootRest.model.OrderRatings;
 import co.nitin.springbootRest.model.OrderRequest;
@@ -27,14 +23,12 @@ import co.nitin.springbootRest.model.Products;
 @SpringBootTest(webEnvironment = WebEnvironment.DEFINED_PORT)
 public class DAOPersistTest {
 
-	private Logger log = LoggerFactory.getLogger(OrderRequestRatingControllerTest.class);
-	
-	@Autowired private OrderRequestRatingController ordController;
+	private Logger log = LoggerFactory.getLogger(DAOPersistTest.class);
+
 	@Autowired private ProductsDAO productdao;
 	@Autowired private OrderRatingsDAO orderRatingsdao;
 	@Autowired private OrderRequestDAO orderRequestdao;
-	@Autowired private OrdersDAO ordersDao;
-
+	
 	@Test
 	public void testProductPersisting() {
 		
